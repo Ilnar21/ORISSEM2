9bea534b914e1e1bc0cf1ba953823414b5a872fb
Ilnar21 <valiullinilnar2005@gmail.com>
2025-05-16 23:41:34 +0300

    project

--- ProjOris2/ProjOris2.Web/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace ProjOris2.Web.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
